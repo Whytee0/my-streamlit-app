f84d939ea3bff9fed274f5000b748aba1785a62e
Whytee0 <whytee163@gmail.com>
2025-04-29 23:06:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 ﻿import streamlit as st
 
 st.title("Streamlit-додаток")
-st.write("Streamlit дозволяє швидко створювати веб-додатки на Python.")
 name = st.text_input("Як тебе звати?")
 if name:
-    st.success(f"Радий тебе бачити, {name}!")
+    st.success(f"Радий вас бачити, {name}!")
