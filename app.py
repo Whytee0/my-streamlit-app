1e4aa7ab8ba14da1f26642958f4109aa75f7454a
Whytee0 <whytee163@gmail.com>
2025-04-29 23:16:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ st.title("Streamlit-додаток")
 st.write("Для перевірки напишіть '1234'")
 name = st.text_input("Write here")
 answer = "1234"
-if name = answer:
+if name == answer:
     st.success(f"Перевірка успішно пройдена")
