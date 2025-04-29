3a91c482b47aa6b68712db75af721f7d189b2212
Whytee0 <whytee163@gmail.com>
2025-04-29 23:13:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 ﻿import streamlit as st
 
 st.title("Streamlit-додаток")
-st.write("Для перевірки напишіть "1234".")
+st.write("Для перевірки напишіть '1234'")
 name = st.text_input("Write here")
 if name:
     st.success(f"Радий вас бачити, {name}!")
