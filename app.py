7e35f4b8c6836457c2b64ef7f021852fa7a9a3b4
Whytee0 <whytee163@gmail.com>
2025-04-29 22:53:16 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,7 @@
+﻿import streamlit as st
+
+st.title("Привіт! Це мій перший Streamlit-додаток")
+st.write("Streamlit дозволяє швидко створювати веб-додатки на Python.")
+name = st.text_input("Як тебе звати?")
+if name:
+    st.success(f"Радий тебе бачити, {name}!")
